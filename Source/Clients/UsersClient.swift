1dab9e3e1fcbf5e687f2388d88c4ac7c75337f63
Eduardo Arenas <eduardo.arenas@gamechanger.io>
2017-08-05 10:58:29 -0400

    Add missing top comment blocks

--- Source/Clients/UsersClient.swift
+++ Source/Clients/UsersClient.swift
@@ -1,3 +1,11 @@
+//
+//  RepositoriesClient.swift
+//  GithubClient
+//
+//  Created by Eduardo Arenas on 8/5/17.
+//  Copyright © 2017 GameChanger. All rights reserved.
+//
+
 import RxSwift
 
 public class UsersClient: ApiClient {

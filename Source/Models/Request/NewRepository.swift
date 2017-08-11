d2b000c5f20428fddb7c7677098134d30128974f
Eduardo Arenas <eduardo.arenas@gamechanger.io>
2017-08-11 19:44:03 -0400

    Add public init to NewRepository

--- Source/Models/Request/NewRepository.swift
+++ Source/Models/Request/NewRepository.swift
@@ -24,6 +24,23 @@ public struct NewRepository: Encodable {
   public let allowSquashMerge: Bool?
   public let allowMergeCommit: Bool?
   public let allowRebaseMerge: Bool?
+
+  public init(name: String, description: String? = nil, homepage: String? = nil, isPrivate: Bool? = nil, hasIssues: Bool? = nil, hasProjects: Bool? = nil, hasWiki: Bool? = nil, teamId: Int? = nil, autoInit: Bool? = nil, gitignoreTemplate: String? = nil, licenseTemplate: String? = nil, allowSquashMerge: Bool? = nil, allowMergeCommit: Bool? = nil, allowRebaseMerge: Bool? = nil) {
+    self.name = name
+    self.description = description
+    self.homepage = homepage
+    self.isPrivate = isPrivate
+    self.hasIssues = hasIssues
+    self.hasProjects = hasProjects
+    self.hasWiki = hasWiki
+    self.teamId = teamId
+    self.autoInit = autoInit
+    self.gitignoreTemplate = gitignoreTemplate
+    self.licenseTemplate = licenseTemplate
+    self.allowSquashMerge = allowSquashMerge
+    self.allowMergeCommit = allowMergeCommit
+    self.allowRebaseMerge = allowRebaseMerge
+  }
 }
 
 extension NewRepository {
